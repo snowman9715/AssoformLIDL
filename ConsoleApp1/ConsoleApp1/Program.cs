79784d49a7eda25d4c4646eb56fe0500b8fa0c13
Giuseppe Ugarelli <giuseppe97.15@gmail.com>
2023-09-03 17:22:54 +0200

    Creando e collegando il db

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                GetDataFromDB();
+                Console.WriteLine("ha funzionato");
+            }catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+        static void GetDataFromDB()
+        {
+            string connectionString = "Data Source= (localdb)\\MSSQLLocalDB;Initial Catalog=GiuseppeUgarelliLIDL;Integrated Security= True";
+            string queryString = "SELECT * FROM Prodotti";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter(queryString, connection);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+            }
+
+
+
+        }
+    }
+}
